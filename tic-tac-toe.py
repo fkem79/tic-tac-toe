367295e09ed63a5cf69100ef2f03d167e554bd50
SON <fkem79@naver.com>
2017-11-25 23:47:27 +0900

    new tic-tac-toe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,20 +1,42 @@
-from tkinter import *
+from tkinter import*
+import tkinter.messagebox
 
+def gameWin() :
+      tkinter.messagebox.showinfo("Win")
+def gameDraw() :
+      tkinter.messagebox.showinfo("Draw")
 def checked(i) :
       global player
+      global checkstate
       button = list[i]
 
       if button["text"] != "     " :
             return
-      button["text"] = player 
+      button["text"] = player
       button["bg"] = "yellow"
-
+      
       if player == "X" :
             player = "O"
             button["bg"] = "yellow"
       else :
             player = "X"
             button["bg"] = "lightgreen"
+
+      row = i // 3
+      col = i % 3
+
+      if list[row * 3]["text"] == list[row * 3 + 1]["text"] == list[row * 3 + 2]["text"] :
+            gameWin()
+      elif list[col]["text"] == list[col + 3]["text"] == list[col + 6]["text"] :
+            gameWin()
+      elif list[0]["text"] == list[4]["text"] == list[8]["text"] != "     " :
+            gameWin()
+      elif list[2]["text"] == list[4]["text"] == list[6]["text"] != "     " :
+            gameWin()
+      elif checkstate==8 :
+            gameDraw()
+
+      checkstate+=1
 
 window = Tk()
 player = "X"
@@ -25,6 +47,7 @@ for i in range(9) :
       b.grid(row=i//3, column=i%3)
       list.append(b)
 
-window.mainloop()
+checkstate=0
 
+window.mainloop()
 
